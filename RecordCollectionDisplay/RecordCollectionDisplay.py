efcad92cc7e7260c88a0069ddf6ae9bdc7a41b3a
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2021-01-15 16:20:59 -0500

    started project

--- RecordCollectionDisplay/RecordCollectionDisplay.py
+++ RecordCollectionDisplay/RecordCollectionDisplay.py
@@ -1,1 +1,6 @@
+import tkinter as tk
 
+welcomeWindow = tk.Tk()
+
+def getList():
+    res = requests.get("https://api.discogs.com/users/thelemon.chris/collection/folders/0/releases?sort=artist&page=")
